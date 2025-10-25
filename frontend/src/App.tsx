afb05dc1115cbed89cae11e358a19ff0f58b95ed
johanfortus <fortusjohan@gmail.com>
2025-10-25 15:18:38 -0400

    Set up backend and gitignore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind CSS Works!
-      </h1>
-    </div>
+	<div>
+	</div>
   )
 }
 
